dcca5d06ba94eaf6ea7e43823844d5f1f48c4b82
Kendall Poindexter <kendall.poindexter@detroitlabs.com>
2018-09-09 16:23:52 -0400

    Book appears in cell when app is run

---
+++ LibraryApp/LibraryApp/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  LibraryApp
+//
+//  Created by Kendall Poindexter on 9/7/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+    let title: String?
+    let author: String?
+}
---
+++ LibraryApp/LibraryApp/BooksInLibrary.swift
@@ -0,0 +1,15 @@
+//
+//  BooksInLibrary.swift
+//  LibraryApp
+//
+//  Created by Kendall Poindexter on 9/7/18.
+//  Copyright © 2018 Kendall Poindexter. All rights reserved.
+//
+
+import Foundation
+
+
+
+struct BookShelf {
+    let books: [Book] = [Book(title: "Between The World And Me", author: "Ta-Nehisi Coates")]
+}
